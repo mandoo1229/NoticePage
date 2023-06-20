36ce709d16a66f12149a0a4be69665bd4bc69754
playground <mandoo1229>
2023-06-20 21:44:39 +0900

    Input창에 입력이 되지 않는 부분을 수정

--- src/Page/NotePage.js
+++ src/Page/NotePage.js
@@ -1,29 +1,34 @@
-import React, { useState} from 'react';
+import React, { useState } from "react";
 
 const NotePage = () => {
-    const [title, setTitle] = useState("");
-    const [check, setCheck] = useState(false);
-    const saveData = () => {
-        const userObj = {title: title};
-        window.localStorage.setItem("setTitle", JSON.stringify(userObj));
-    }
-    const callData = (e) => {
-        setTitle(e.target.value);
-        setCheck(false);
-    }
-    return (
-        <div>
-            <input
-                name="title"
-                value={title}
-                onChange={onchange}
-                placeholder="제목을 입력해주세요."
-            />
-            <button onClick={saveData}>저장하기</button>
-            <button onChange={callData}>불러오기</button>
-            {check ? <p>{window.localStorage.getItem("title")}</p> : <></>}
-        </div>
-    );
+  const [title, setTitle] = useState("");
+  const [check, setCheck] = useState(false);
+  const saveData = () => {
+    const userObj = { title: title };
+    window.localStorage.setItem("setTitle", JSON.stringify(userObj));
+  };
+
+  const callData = () => {
+    setCheck(true);
+  };
+  
+  const onChange = (e) => {
+    setTitle(e.target.value);
+    setCheck(false);
+  };
+  return (
+    <div>
+      <input
+        name="title"
+        value={title}
+        onChange={onChange}
+        placeholder="제목을 입력해주세요."
+      />
+      <button onClick={saveData}>저장하기</button>
+      <button onChange={callData}>불러오기</button>
+      {check ? <p>{window.localStorage.getItem("title")}</p> : <></>}
+    </div>
+  );
 };
 
 export default NotePage;
