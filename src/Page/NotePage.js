e2130f53e0330e98a593277b14ebe77dd9b626a6
playground <mandoo1229>
2023-06-21 14:13:31 +0900

    Fix : localStorage에 JSON타입으로 올라가게 코드를 수정함

--- src/Page/NotePage.js
+++ src/Page/NotePage.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 function NotePage() {
   const [todo, setTodo] = useState([{ name: '' }]);
-  let [inputValue, setInputValue] = useState('');
-  let [newTodo, setNewTodo] = useState([{ name: '' }]);
+  const [inputValue, setInputValue] = useState('');
+  const [newTodo, setNewTodo] = useState({ name: '' });
 
   const inputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  useEffect(() => setNewTodo({ name: inputValue }), [inputValue]);
+  useEffect(() => {
+    const storedTodo = JSON.parse(window.localStorage.getItem('todoInLocal'));
+    if (storedTodo) {
+      setTodo(storedTodo);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('todoInLocal', JSON.stringify(todo));
+  }, [todo]);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -18,15 +27,11 @@ function NotePage() {
     setInputValue('');
   };
 
-  const todsMap = todo.map((todoItem, i) => <p ket={i}>{todoItem.name}</p>);
-
-  useEffect(() => {
-    window.localStorage.getItem('todoInLocal', JSON.stringify(todo));
-  }, [todo]);
+  const todosMap = todo.map((todoItem, i) => <p key={i}>{todoItem.name}</p>);
 
   return (
     <div>
-      name: <div>{todsMap}</div>
+      name: <div>{todosMap}</div>
       <form onSubmit={onSubmit}>
         <input value={inputValue} onChange={inputChange}></input>
         <button>저장</button>
